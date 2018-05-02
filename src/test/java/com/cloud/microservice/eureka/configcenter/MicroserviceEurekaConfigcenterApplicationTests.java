53418f06c2904ce9aecf1034cf4235091c710c2f
patrickjia <jiapengcheng_java@163.com>
2018-05-02 18:32:17 +0800

    commit config-center

---
+++ src/test/java/com/cloud/microservice/eureka/configcenter/MicroserviceEurekaConfigcenterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloud.microservice.eureka.configcenter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceEurekaConfigcenterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
